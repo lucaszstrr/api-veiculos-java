6b0ce487a7fae177ed95f9a9ccceb196563d31cb
lucaszstrr <lucaaszstr@gmail.com>
2026-02-12 17:28:59 -0300

    model criada

---
+++ src/main/java/com/veiculos/CadastroDeVeiculos/VeiculoModel.java
@@ -0,0 +1,48 @@
+package com.veiculos.CadastroDeVeiculos;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_veiculos")
+public class VeiculoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String marca;
+    String modelo;
+    int ano;
+
+    public VeiculoModel() {
+    }
+
+    public VeiculoModel(String marca, String modelo, int ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
