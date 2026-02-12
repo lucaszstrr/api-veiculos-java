e970847bdec69feec9eb17fb4fd5c16202bb712d
lucaszstrr <lucaaszstr@gmail.com>
2026-02-12 16:16:26 -0300

    config inicial do projeto

---
+++ src/main/java/com/veiculos/CadastroDeVeiculos/CadastroDeVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.veiculos.CadastroDeVeiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeVeiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veiculos/CadastroDeVeiculos/CadastroDeVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veiculos.CadastroDeVeiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
