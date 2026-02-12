5b1503db7fcf9c02b1459b80765104f0165b456e
lucaszstrr <lucaaszstr@gmail.com>
2026-02-12 17:31:53 -0300

    veiculo controller

---
+++ src/main/java/com/veiculos/CadastroDeVeiculos/VeiculoController.java
@@ -0,0 +1,16 @@
+package com.veiculos.CadastroDeVeiculos;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class VeiculoController {
+
+    @GetMapping("/")
+    public String teste() {
+        return "Boas-vindas!";
+    }
+
+}
