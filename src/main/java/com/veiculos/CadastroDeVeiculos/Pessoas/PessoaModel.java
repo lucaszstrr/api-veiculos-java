c3d03f3fc68b795a7c73437b5be4a30f0f92c6bc
lucaszstrr <lucaaszstr@gmail.com>
2026-02-16 12:06:58 -0300

    anotations do lombok adicionadas nas models

--- src/main/java/com/veiculos/CadastroDeVeiculos/Pessoas/PessoaModel.java
+++ src/main/java/com/veiculos/CadastroDeVeiculos/Pessoas/PessoaModel.java
@@ -2,43 +2,26 @@ package com.veiculos.CadastroDeVeiculos.Pessoas;
 
 import com.veiculos.CadastroDeVeiculos.Veiculos.VeiculoModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_pessoas")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class PessoaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
 
     @OneToMany(mappedBy = "pessoaId")
     private List<VeiculoModel> veiculoId;
 
-    public PessoaModel() {
-    }
-
-    public PessoaModel(String nome, String email) {
-        this.nome = nome;
-        this.email = email;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
--- src/main/java/com/veiculos/CadastroDeVeiculos/Veiculos/VeiculoModel.java
+++ src/main/java/com/veiculos/CadastroDeVeiculos/Veiculos/VeiculoModel.java
@@ -2,52 +2,29 @@ package com.veiculos.CadastroDeVeiculos.Veiculos;
 
 import com.veiculos.CadastroDeVeiculos.Pessoas.PessoaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_veiculos")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class VeiculoModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String marca;
+
     private String modelo;
+
     private int ano;
 
     @ManyToOne
     @JoinColumn(name = "pessoa_id")
     private PessoaModel pessoaId;
 
-    public VeiculoModel() {
-    }
-
-    public VeiculoModel(String marca, String modelo, int ano) {
-        this.marca = marca;
-        this.modelo = modelo;
-        this.ano = ano;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public int getAno() {
-        return ano;
-    }
-
-    public void setAno(int ano) {
-        this.ano = ano;
-    }
 }
