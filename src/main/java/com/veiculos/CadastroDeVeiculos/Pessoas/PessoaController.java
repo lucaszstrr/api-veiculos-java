0b0cd027933a7fad30501c89d13eeec479749599
lucaszstrr <lucaaszstr@gmail.com>
2026-02-18 16:23:43 -0300

    feat: cria instancia do service

--- src/main/java/com/veiculos/CadastroDeVeiculos/Pessoas/PessoaController.java
+++ src/main/java/com/veiculos/CadastroDeVeiculos/Pessoas/PessoaController.java
@@ -1,11 +1,17 @@
 package com.veiculos.CadastroDeVeiculos.Pessoas;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
 public class PessoaController {
+
+    @Autowired
+    private PessoaService pessoaService;
 
     @PostMapping("/pessoa")
     public void criarPessoa() {
@@ -18,8 +24,8 @@ public class PessoaController {
     }
 
     @GetMapping("/pessoas")
-    public String mostrarTodos() {
-        return "Todas as pessoas";
+    public List<PessoaModel> mostrarTodos() {
+        return pessoaService.listarPessoas();
     }
 
     @PutMapping("/pessoa/{id}")
