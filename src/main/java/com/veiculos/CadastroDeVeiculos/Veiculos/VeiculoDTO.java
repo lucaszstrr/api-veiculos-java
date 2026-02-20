27d11cba13796f68f946ce484946abd64f0e13b8
lucaszstrr <lucaaszstr@gmail.com>
2026-02-20 13:58:09 -0300

    feat: implementa o VeiculoDTO

---
+++ src/main/java/com/veiculos/CadastroDeVeiculos/Veiculos/VeiculoDTO.java
@@ -0,0 +1,21 @@
+package com.veiculos.CadastroDeVeiculos.Veiculos;
+
+import com.veiculos.CadastroDeVeiculos.Pessoas.PessoaModel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class VeiculoDTO {
+
+    private Long id;
+    private String marca;
+    private String modelo;
+    private int ano;
+    private PessoaModel pessoaId;
+
+}
